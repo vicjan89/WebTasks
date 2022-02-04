29d0ed262f595ae777a235bc1cdd406022a06645
Yanush Viktar <vicjan89@gmail.com>
2022-02-04 16:11:51 +0300

    '3'

--- WebTasks/settings.py
+++ WebTasks/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['dry-sands-57378.herokuapp.com']
+ALLOWED_HOSTS = ['morning-bayou-54560.herokuapp.com']
 
 
 # Application definition
