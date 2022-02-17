aecc5d24457a5d77777f8483ec39a699e8b21b92
Yanush Viktar <vicjan89@gmail.com>
2022-02-17 21:47:54 +0300

    Авторизация

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,26 @@
 from django.urls import path
-
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView
+from django.contrib.auth.views import PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from .views import index, by_label, TaskCreateView, attachment, TaskDetaiView, TaskUpdateView
 
 urlpatterns = [
     path('', index, name='index'),
+    path('accounts/login/', LoginView.as_view(next_page='index'), name='login'),
+    path('accounts/logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('accounts/password_change/', PasswordChangeView.as_view(template_name='registration/change_password.html'),
+         name='password_change'),
+    path('accounts/password_change/done/', PasswordChangeDoneView.as_view(template_name='registration/password_changed.html'),
+         name='password_change_done'),
+    path('accounts/password_reset/', PasswordResetView.as_view(template_name='registration/reset_password.html',
+                                                               subject_template_name='registration/reset_subject.txt',
+                                                               email_template_name='registration/reset_email.txt'),
+         name='password_reset'),
+    path('accounts/password_reset/done', PasswordResetDoneView.as_view(template_name='registration/email_sent.html'),
+         name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(template_name='registration/confirm_password.html'),
+         name='password_reset_confirm'),
+    path('accounts/reset/done/', PasswordResetCompleteView.as_view(template_name='registration/password_confirmed.html'),
+         name='password_reset_complete'),
     path('attachment/', attachment, name='attachment'),
     path('add/', TaskCreateView.as_view(), name='add'),
     path('task/<int:pk>/edit/', TaskUpdateView.as_view(), name='edit'),
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
 from .models import Task, Label, Attachment
 from .forms import TaskForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class TaskCreateView(CreateView):
+class TaskCreateView(LoginRequiredMixin, CreateView):
     template_name = 'tasks/create.html'
     form_class = TaskForm
     success_url = reverse_lazy('index')
@@ -16,19 +18,21 @@ class TaskCreateView(CreateView):
         context['label'] = Label.objects.all()
         return context
 
-class TaskDetaiView(DetailView):
+class TaskDetaiView(LoginRequiredMixin, DetailView):
     model = Task
     template_name = 'tasks/detail.html'
 
+@login_required
 def index(request):
-    tasks_list = Task.objects.all()
-    lbls = Label.objects.all()
-    context = {
-        'tasks_list': tasks_list,
-        'lbls': lbls
-    }
-    return render(request, 'tasks/index.html', context)
+        tasks_list = Task.objects.all()
+        lbls = Label.objects.all()
+        context = {
+            'tasks_list': tasks_list,
+            'lbls': lbls
+        }
+        return render(request, 'tasks/index.html', context)
 
+@login_required
 def by_label(request, label_id):
     lbls = Label.objects.all()
     current_label = Label.objects.get(pk=label_id)
@@ -36,12 +40,13 @@ def by_label(request, label_id):
     context = {'tsk': tsk, 'lbls': lbls, 'current_label': current_label}
     return render(request, 'tasks/by_label.html', context)
 
+@login_required
 def attachment(request):
     attach = Attachment.objects.all()
     context = {'attach': attach}
     return render(request, 'tasks/attachment.html', context)
 
-class TaskUpdateView(UpdateView):
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = Task
     form_class = TaskForm
     template_name = 'tasks/create.html'
@@ -52,7 +57,7 @@ class TaskUpdateView(UpdateView):
         context['label'] = Label.objects.all()
         return context
 
-class TaskDeleteView(DeleteView):
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     form_class = TaskForm
     template_name = 'tasks/create.html'
